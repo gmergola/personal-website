2ece4ec1177254072725ad9befc630ceabdf32b2
Genna Mergola <gennamergola@gmail.com>
2020-05-30 15:43:17 -0700

    added styling and responsiveness

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home(){
     <div>
       <p className="Home-intro">Hi, my name is</p>
       <h1 className="Home-name">Genna</h1>
-      <h3 className="Home-info">I build things for the web</h3>
+      <h3 className="Home-info">I build things for the web...</h3>
     </div>
   )
 }
