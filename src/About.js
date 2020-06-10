d4f23943517da88d398f50944cd28ce143523e4e
Genna Mergola <gennamergola@gmail.com>
2020-06-10 14:34:05 -0700

    adding resume link button

--- src/About.js
+++ src/About.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './About.css';
-import resume from './images/resume.png';
 
-function About(){
+function About() {
   return (
     <div>
       <h1 className="About-title">About Me</h1>
       <p className="About-info">
         I am a software engineer based in San Francisco, CA
       </p>
-      <img className="About-resume" src={resume} alt="resume"/>
+      <button className="About-resume" >
+        <a className="About-resume-link" href="https://docs.google.com/document/d/1uVXYcAsbLHP8MNTOhjnVjyhFYnJltd1vMC4k34muxbw/edit">Resume</a>
+      </button>
     </div>
   )
 }
