f7447e069e8fb8c74f5d712f95f3f4540813823a
Genna Mergola <gennamergola@gmail.com>
2020-06-10 20:10:50 -0700

    adding to projects page and clearing readme

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 ## Welcome to my personal site
 
-# Home Page
-![website homepage](./screen-shots/homepage.png)
-# About Me
-![website about](./screen-shots/about.png)
-# Contact
-![website contact](./screen-shots/contact.png)
-# Projects page
-![website contact](./screen-shots/projects.png)
--- src/Projects.js
+++ src/Projects.js
@@ -6,14 +6,15 @@ function Projects(){
   return (
     <div id="Project-div">
       <h1 id="Project-title" className="Project-title" >Some Apps I've built...</h1>
-      {/* <img className="Project-fac-arrow" src={faceArrow} alt=""/> */}
       <br/>
       <p className="Project-name">Jobly</p>
       <button className="Project-btn"><a className="Project-link"href="https://github.com/gmergola/react-jobly">Github Link </a></button>
-      <p className="Project-info" >Built using: React, Express, and PostgreSQL </p>
+      <p className="Project-info" >A site for users to find and apply for jobs</p>
+      <p className="Project-info" ><b>Built using: </b> React, Express, and PostgreSQL </p>
       <p className="Project-name">Warbler</p>
       <button className="Project-btn"><a className="Project-link"href="https://github.com/gmergola/Warbler">Github Link </a></button>
-      <p className="Project-info">A Twitter clone application built using: Python, Flask, and PostgreSQL</p>
+      <p className="Project-info">A Twitter clone application</p>
+      <p className="Project-info"><b>Built using: </b> Python, Flask, and PostgreSQL</p>
       <div className="Project-bottom"></div>
     </div>
   )
