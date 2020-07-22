060db765c4e3b8f4e8d176075d24e02306a44554
Genna Mergola <gennamergola@gmail.com>
2020-07-22 15:49:40 -0700

    adding questions to projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Games from './Games';
 import './Projects.css';
 import jobly from './images/jobly-home.png';
 import microblog from './images/microblog-home.png';
 import warbler from './images/warbler-home.png';
-import { Link } from "react-scroll";
+import jeopardy from './images/jeopardy-home.png';
+import guessingGame from './images/guessing-game.png';
+import questions from './images/questions.png';
 
 function Projects() {
 
@@ -25,6 +26,20 @@ function Projects() {
           </p>
           <button className="Project-btn"><a className="Project-link" href="https://github.com/gmergola/Microblog">Github Link </a></button>
           <button className="Project-btn"><a className="Project-link" href="https://nervous-agnesi-2c7074.netlify.app/">Live App Demo</a></button>
+        </div>
+      </div>
+
+      <div className="Project-container">
+        <img className="Projects-picture" src={questions} alt="questions" />
+        <div className="Project-context">
+          <p className="Project-name">Questions!</p>
+          <h5 className="Project-info">Built using: React, Express, and PostgreSQL</h5>
+          <p className="Project-info">
+            Questions is a full stack Family Feud inspired game. The user can choose a question to answer.
+            The game is built for two teams. Each team has the chance to answer questions and rack up points!
+          </p>
+          <button className="Project-btn"><a className="Project-link" href="https://github.com/gmergola/Questions">Github Link </a></button>
+          <div className="Project-link Project-btn">Live App Demo coming soon!</div>
         </div>
       </div>
 
@@ -65,28 +80,34 @@ function Projects() {
         </div>
       </div>
 
+      <div className="Project-container">
+        <img className="Projects-picture" src={jeopardy} alt="jeopardy-home" />
+        <div className="Project-context">
+          <p className="Project-name">Jeopardy!</p>
+          <h5 className="Project-info">Built using: Javascript, JQuery, HTML, and CSS</h5>
+          <p className="Project-info">
+            A game to play Jeopardy! Jeopardy is a frontend application built using Javascript. It makes an axios GET
+            request to the Jeopardy API to get questions and answers. Try it out by clicking the button below!
+          </p>
+          <button className="Project-btn"><a className="Project-link" href="http://physical-achieve.surge.sh/">Live App Demo </a></button>
+        </div>
+      </div>
+
+      <div className="Project-container">
+        <img className="Projects-picture" src={guessingGame} alt="guessing-game" />
+        <div className="Project-context">
+          <p className="Project-name">Guessing Game!</p>
+          <h5 className="Project-info">Built using: Vanilla Javascript, HTML, and CSS</h5>
+          <p className="Project-info">
+            Guess a number between 1 and 100... The game will choose a random number for you to try and guess, it will let you know
+            if your guess was to high, too low, or just right!
+          </p>
+          <button className="Project-btn"><a className="Project-link" href="https://github.com/gmergola/guessing-game">Github Link </a></button>
+          <button className="Project-btn"><a className="Project-link" href="http://tan-market.surge.sh/">Live App Demo </a></button>
+        </div>
+      </div>
+
       <div className="Project-bottom"></div>
-      <Link
-        activeClass="active"
-        className="Home-projects-btn"
-        to="Games-title"
-        spy={true}
-        smooth={true}
-        duration={1500}>
-        Play some games I've built
-      </Link>
-
-      <Link
-        activeClass="active"
-        className="Home-projects-btn"
-        to="NavBar-container"
-        spy={true}
-        smooth={true}
-        duration={2000}>
-        Scroll to top
-      </Link>
-
-      <Games />
     </div>
   )
 }
