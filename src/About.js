875f180206875ed140ecc40c546c7b72e220d1aa
Genna Mergola <gennamergola@gmail.com>
2020-08-25 16:57:31 -0700

    changed about page

--- src/About.js
+++ src/About.js
@@ -1,72 +1,27 @@
 import React from 'react';
 import './About.css';
 
-/**TODO: margin check description */
 function About() {
   return (
     <div>
       <h1 className="About-title">About Me</h1>
-      <h3 className="About-SE"><i>I am a software engineer based in San Francisco, CA</i></h3>
       <div className="About-info">
-        <h4 className="About-work">WORK EXPERIENCE</h4>
-
-        <div className="About-info-container">
-          <div className="About-info-pre">
-            <h3>Software Engineer Intern @ Clever Counsel</h3>
-            <p>May 2020 - June 2020 // San Francisco </p>
-            <p><i>Clever Counsel is a tech start-up Leveraging technology to address the justice gap.</i></p>
-          </div>
-          <div className="About-info-body">
-            <p>‣ Developed full stack user friendly functionality for the application across
-              multiple areas using React, Flask with SQLAlchemy, mySQL, and AWS</p>
-            <p>‣ Implemented and fully designed the frontend and backend of the search
-              functionality for the application used in multiple components</p>
-            <p>‣ Doubled frontend test coverage using jest with api mocking</p>
-            <p>‣ Designed the frontend and backend functionality  for a user to write a note
-              about a case</p>
-            <p>‣ Collaborated with a team of five engineers to upgrade the fullstack code base
-              for easier readability, ensuring long term success</p>
-          </div>
-        </div>
-        <br />
-
-        <div className="About-info-container">
-          <div className="About-info-pre">
-            <h3>Yoga Instructor @ Corepower Yoga </h3>
-            <p>2017 - Present // San Francisco, New York City, Boston  </p>
-            <p>
-              <i>Corepower Yoga’s mission is to show the world the incredible
-              life-changing things that happen when you root an intensely physical
-              workout in the mindfulness of yoga.</i>
-            </p>
-          </div>
-          <div className="About-info-body">
-            <p>‣ Led groups of 20-50 people through an hour class while exercising my ability to
-            keep everyone on track as well as controlling the music and temperature of the room.</p>
-            <p>‣ Lead a 5 week Yoga Sculpt teacher training to approximately 30 students in NYC, where
-            I recruited 7 out of 14 participants.</p>
-            <p>‣ Was given a 10% raise as a result of demonstrating great communication and customer
-            service skills between myself and the clients.</p>
-          </div>
-        </div>
-        <br />
-
-        <div className="About-info-container">
-          <div className="About-info-pre">
-            <h3>Professional Dancer @ Artichoke Dance Company  </h3>
-            <p>2016 - 2019 // New York City</p>
-            <p>
-              <i>Artichoke Dance Company offers unique public performances and educational art experiences.
-              Through the interactive nature of dance, they help people become more physically, socially,
-                and environmentally conscious.</i>
-            </p>
-          </div>
-          <div className="About-info-body">
-            <p>‣ Performed for over a thousand audience members across NYC at sustainability conscious and environmentally
-            aware events.</p>
-            <p>‣ Created material for the whole company to use in over 20+ performances around NYC.</p>
-          </div>
-        </div>
+        <h4 className="About-work"><i>Yogi turned Software Engineer</i></h4>
+        <p className="About-info-container">
+          Through my journey as a professional modern dancer and yoga/fitness instructor I found an interest in learning to code.
+          Finally, I decided it was time I stop thinking about it and start learning. While performing professionally
+          for <b><a href="https://www.artichokedance.org/"> Artichoke Dance Company</a></b>, a company member I danced with was a full time
+          Software Engineer while dancing professionally part time. Once I started to teach myself to code I would often ask him
+          questions on what I had just learned. I thought it was so great to be asking coding advice from a fellow dancer, I never
+          would have thought! I have also had the pleasure of connecting with so many awesome professionals in the tech industry through
+          my career in yoga and fitness with <b><a href="https://www.corepoweryoga.com/">Corepower Yoga</a></b> and <b><a href="https://www.janedo.com/">
+          Jane DO</a></b>. Shortly after I began my coding journey, my husband and I moved to San Francisco from NYC due to a new
+          job opportunity. While in San Francisco, I found <b><a href="https://www.rithmschool.com/">Rithm School</a></b> coding bootcamp and
+          after doing my research I knew I had to interview. I am now a graduate of Rithm school and ready to start my new journey in
+          the tech industry! My journey to becoming a Software Engineer has definitely been a non-traditional one, but I wouldn't
+          change it. It has made me the engineer and person I am today. I am passionate about coding as well as physical and mental health,
+          if you share either of my passions, or just want to chat, please reach out to me through my contact page, I'd love to hear from you!
+      </p>
       </div>
     </div>
   )
