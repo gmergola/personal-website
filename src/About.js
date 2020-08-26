8282cbbc453f86ec380e9a8885a385ca771e6215
Genna Mergola <gennamergola@gmail.com>
2020-08-25 17:05:38 -0700

    updated about page

--- src/About.js
+++ src/About.js
@@ -13,14 +13,14 @@ function About() {
           for <b><a href="https://www.artichokedance.org/"> Artichoke Dance Company</a></b>, a company member I danced with was a full time
           Software Engineer while dancing professionally part time. Once I started to teach myself to code I would often ask him
           questions on what I had just learned. I thought it was so great to be asking coding advice from a fellow dancer, I never
-          would have thought! I have also had the pleasure of connecting with so many awesome professionals in the tech industry through
-          my career in yoga and fitness with <b><a href="https://www.corepoweryoga.com/">Corepower Yoga</a></b> and <b><a href="https://www.janedo.com/">
-          Jane DO</a></b>. Shortly after I began my coding journey, my husband and I moved to San Francisco from NYC due to a new
-          job opportunity. While in San Francisco, I found <b><a href="https://www.rithmschool.com/">Rithm School</a></b> coding bootcamp and
-          after doing my research I knew I had to interview. I am now a graduate of Rithm school and ready to start my new journey in
+          would have thought! Also, through my career in yoga and fitness with <b><a href="https://www.corepoweryoga.com/">Corepower Yoga</a></b>
+          and <b><a href="https://www.janedo.com/">Jane DO</a></b> I have had the pleasure of connecting with so many awesome professionals
+          in the tech industry I knew coding was the perfect next step for me. Shortly after I began my coding journey, my husband and I moved
+          to San Francisco from NYC due to a new job opportunity. While in San Francisco, I found <b><a href="https://www.rithmschool.com/">Rithm School</a></b>
+          coding bootcamp and after doing my research I knew I had to interview. I am now a graduate of Rithm school and ready to start my new journey in
           the tech industry! My journey to becoming a Software Engineer has definitely been a non-traditional one, but I wouldn't
           change it. It has made me the engineer and person I am today. I am passionate about coding as well as physical and mental health,
-          if you share either of my passions, or just want to chat, please reach out to me through my contact page, I'd love to hear from you!
+          if you share any of my passions, or just want to chat, please reach out to me through my contact page, I'd love to hear from you!
       </p>
       </div>
     </div>
